60dbf75299bd872e37e00ce10c978322ca74b3c8
VWRoli <fustroli@gmail.com>
2022-12-03 15:05:04 +0100

    refactor: fetching data

--- vanilla-js-todo/main.js
+++ vanilla-js-todo/main.js
@@ -7,9 +7,6 @@ const toggleBtnEl = document.querySelector('#toggle-button');
 const formEl = document.querySelector('#add-form');
 const contentEl = document.querySelector('#content');
 
-let rendered = fas;
-let todos = [];
-
 toggleBtnEl.addEventListener('click', () => {
   //Change btn color
   toggleBtnEl.classList.toggle('primary');
@@ -22,32 +19,32 @@ toggleBtnEl.addEventListener('click', () => {
 });
 
 //API
-const fetchTodos = async () =>
-  fetch(`${API_URL}?_limit=5`)
-    .then((response) => response.json())
-    .then((json) => {
-      rendered = true;
-    });
-
-fetchTodos();
-console.log(todos);
-
-todos.map((t) => renderData(t));
+(async () => {
+  let todos;
+  const fetchTodos = async () => {
+    const res = await fetch(`${API_URL}?_limit=5`);
+    const data = await res.json();
+    todos = data;
+    return data;
+  };
+  await fetchTodos();
+  todos.map((t) => renderData(t));
+})();
 
 const renderData = (data) => {
   const html = `<div class="todo">
-      <div class="todo-wrapper">
-        <div class="todo-title">
-          <label for="completed"
-            ><span
-              ><input
-                type="checkbox"
-                id="completed"
-                name="completed"
-                ${data.completed ? 'checked' : ''} /></span
-          ></label>
-          <p class="empty-list">${data.title}</p>
-        </div>
+  <div class="todo-wrapper">
+  <div class="todo-title">
+  <label for="completed"
+  ><span
+  ><input
+  type="checkbox"
+  id="completed"
+  name="completed"
+  ${data.completed ? 'checked' : ''} /></span
+  ></label>
+  <p class="empty-list">${data.title}</p>
+  </div>
         <div class="todo-actions"><button type="button">x</button></div>
       </div>
     </div>`;
