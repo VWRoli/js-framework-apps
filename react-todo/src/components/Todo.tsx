8d850dc7261442a077bb2796dcc1c7c477e20dcd
VWRoli <fustroli@gmail.com>
2022-12-03 08:20:19 +0100

    feat: edit button

--- react-todo/src/components/Todo.tsx
+++ react-todo/src/components/Todo.tsx
@@ -2,7 +2,9 @@ import { CardContent, Typography } from '@mui/material';
 import Card from '@mui/material/Card';
 import { TodoType } from '../types';
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import { red } from '@mui/material/colors';
+import { Box } from '@mui/system';
 
 interface Props {
   todo: TodoType;
@@ -12,7 +14,10 @@ const Todo: React.FC<Props> = (props) => {
     <Card sx={{ my: 2, bgcolor: '#f5f5f5' }}>
       <CardContent sx={{ display: 'flex', justifyContent: 'space-between' }}>
         <Typography>{props.todo.title}</Typography>
-        <CloseOutlinedIcon sx={{ color: red[500] }} />
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <EditOutlinedIcon sx={{ color: 'primary.main', cursor: 'pointer' }} />
+          <CloseOutlinedIcon sx={{ color: red[500], cursor: 'pointer' }} />
+        </Box>
       </CardContent>
     </Card>
   );
