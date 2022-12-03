c05c0b62166e409134d6acb71f027eceb81a6e21
VWRoli <fustroli@gmail.com>
2022-12-03 13:29:45 +0100

    feat: toggle button

--- vanilla-js-todo/main.js
+++ vanilla-js-todo/main.js
@@ -1,1 +1,11 @@
-console.log('start');
+'use strict';
+
+//Variables
+const toggleBtnEl = document.querySelector('#toggle-button');
+const formEl = document.querySelector('#add-form');
+
+toggleBtnEl.addEventListener('click', () => {
+  //Change btn color
+  toggleBtnEl.classList.toggle('error');
+  formEl.classList.toggle('visible');
+});
