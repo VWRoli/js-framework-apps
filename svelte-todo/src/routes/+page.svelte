56df9ff01e43beeefe6ea6dc09838a2810ef740e
VWRoli <fustroli@gmail.com>
2022-12-15 08:04:31 +0100

    feat: delete todo

--- svelte-todo/src/routes/+page.svelte
+++ svelte-todo/src/routes/+page.svelte
@@ -1,13 +1,34 @@
 <script lang="ts">
 	import Header from '../components/Header.svelte';
 	import Form from '../components/Form.svelte';
+	import Item from '../components/Item.svelte';
+	import { API_URL } from '../constants/index';
+	import { onMount } from 'svelte';
+	import type { TodoType } from '../types/index';
 
-	let isOpen = false;
+	let todos: any = [];
+	let isOpen = true;
 
 	function toggleOpen() {
 		isOpen = !isOpen;
 	}
+	async function deleteTodo(id: number) {
+		try {
+			await fetch(`${API_URL}/${id}`, { method: 'DELETE' });
+			todos = todos.filter((t: TodoType) => t.id !== id);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	onMount(async function (): Promise<void> {
+		const response = await fetch(`${API_URL}?_limit=5`);
+		const data = await response.json();
+		todos = data;
+	});
 </script>
 
 <Header {isOpen} {toggleOpen} />
 {#if isOpen}<Form />{/if}
+{#each todos as todo}
+	<Item {todo} {deleteTodo} />
+{/each}
